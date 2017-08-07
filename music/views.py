88c93e139554e2775e7a8ebaec44024933e821ff
swetakumari95 <sweta191995@gmail.com>
2017-08-08 01:38:57 +0530

    Modified details page, added 404 error

--- music/views.py
+++ music/views.py
@@ -1,15 +1,16 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 from .models import Album
+from django.http import Http404
 
 # Create your views here.
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/'+str(album.id)+'/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album id: "+ str(album_id) +"</h2>" )
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album does not exist!')
+    return render(request, 'music/detail.html', {'album' : album})
